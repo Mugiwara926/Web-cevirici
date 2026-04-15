47fe4dd27791f7555c14b1cf3429dac3b9a27ce4
Mugiwara926 <120409215+Mugiwara926@users.noreply.github.com>
2026-04-15 12:53:54 +0300

    a

--- background.js
+++ background.js
@@ -6,43 +6,27 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "fetch_image") {
-    fetch(request.url)
-      .then(res => {
-        if (!res.ok) throw new Error("Resim indirilemedi");
-        return res.blob();
-      })
-      .then(blob => {
-        const reader = new FileReader();
-        reader.onloadend = () => sendResponse({ data: reader.result });
-        reader.readAsDataURL(blob);
-      })
-      .catch(err => sendResponse({ error: err.message }));
-    return true; 
-  }
-
-  if (request.action === "translate") {
-    const safeText = encodeURIComponent(request.text.trim());
-    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=tr&dt=t&q=${safeText}`;
-    
-    fetch(url)
-      .then(res => res.json())
-      .then(json => {
-        if (json && json[0]) {
-          const translated = json[0].map(s => s[0]).join("");
-          sendResponse({ translated });
-        } else {
-          sendResponse({ error: "Çeviri verisi boş" });
-        }
-      })
-      .catch(() => sendResponse({ error: "Google API bağlantı hatası" }));
-    return true;
-  }
-});
-
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "translate-this-image") {
     chrome.tabs.sendMessage(tab.id, { action: "manual_process", url: info.srcUrl });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // Python sunucusuna güvenli arka plan bağlantısı
+  if (request.action === "process_with_api") {
+    fetch("http://127.0.0.1:8000/process-image", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ image_base64: request.base64_data })
+    })
+    .then(res => {
+        if (!res.ok) throw new Error("API Hatası: " + res.status);
+        return res.json();
+    })
+    .then(data => sendResponse(data))
+    .catch(err => sendResponse({ error: err.message }));
+    
+    return true; // Asenkron yanıt için kritik
   }
 });
